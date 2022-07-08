e87f6419b14c98efbbae36fa5f29c2c59a9df3ed
Eddy <edwardyue93@gmail.com>
2022-07-09 00:50:51 +0200

    Whatsapp button added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,9 @@ import { PageProps } from '../common/types'
 import { PageLayout } from '../components/layout/PageLayout'
 // import { homeContent } from '../content/home'
 import styles from './Home.module.css'
+import { Button } from '@mui/material'
+import Link from 'next/link'
+import { allRoutes } from '../common/routes'
 
 export const Home: FC<PageProps> = (pageProps) => {
   const { lang } = pageProps
@@ -12,11 +15,11 @@ export const Home: FC<PageProps> = (pageProps) => {
   return (
     <PageLayout {...pageProps}>
       <Head>
-        {/* <title>{`V&P | ${content.title}`}</title> */}
+        <title>{`V&P | ${'Home'/*content.title*/}`}</title> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
       </Head>
       <div>
         <div className={styles.homeVideoContainer}>
-          <h1 className={styles.homeTitle}>Weddings in Mallorca</h1>
           <video
             className={styles.homeVideo}
             loop
@@ -27,9 +30,30 @@ export const Home: FC<PageProps> = (pageProps) => {
               src='/videos/example.mp4'
               type='video/mp4'
             />
-            Your browser does not support the video tag.
           </video>
+          <h1 className={styles.homeTitle}>Weddings in Mallorca</h1>
+          <Link href={allRoutes.CONTACT.path[lang]}>
+            <a>
+              <Button
+                className={styles.homeButton}
+                variant='contained'
+              >
+                More Info
+              </Button>
+            </a>
+          </Link>
         </div>
+        <Link
+          href="https://wa.me/34620844085"
+        >
+          <a
+            className={styles.whatsappFloat}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-whatsapp whatsapp-icon"></i>
+          </a>
+        </Link>
       </div>
     </PageLayout>
   )
